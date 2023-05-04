76f2e5ef7590e09c38ecf3d8e416356056a78d03
Francisco Feix <111092720+francisco0421@users.noreply.github.com>
2023-05-03 21:48:36 -0300

    Add files via upload

---
+++ biblioteca/src/main/java/br/com/senac/biblioteca/Biblioteca.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.senac.biblioteca;
+
+/**
+ *
+ * @author Aluno TDS
+ */
+public class Biblioteca {
+
+    public static void main(String[] args) {
+        FrameHome home = new FrameHome();
+        home.setVisible(true);
+    }
+}
